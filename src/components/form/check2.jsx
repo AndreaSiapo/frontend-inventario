86ae7f98a279988c3962b8af2ebd768eb0f121fc
AndreaSB <andreasiapob@gmail.com>
2025-11-27 22:42:28 -0500

    Check2
    corrección de estilos

--- src/components/form/check2.jsx
+++ src/components/form/check2.jsx
@@ -16,7 +16,7 @@ export default function CheckboxBall({
         checked={checked}
         onChange={onChange}
         className="sr-only peer"/>
-        <div className="relative w-9 h-5 bg-red-500 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+        <div className="relative w-9 h-5 bg-red-500 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 rtl:after:left-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
       {label && (
         <label htmlFor={id} className={classLabel2}>
           {label}
